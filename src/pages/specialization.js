1eedd047226e97bb4e18d8d2795abc5e1d4b4a80
First First Design <contact@firstfirst.design>
2023-11-02 13:36:25 +0100

    Adding Pages

---
+++ src/pages/specialization.js
@@ -0,0 +1,14 @@
+import * as React from "react";
+import Layout from "../components/layout/layout";
+
+const Specialization = () => {
+  return (
+    <Layout pageTitle="Schwerpunkte">
+      <p>Schwerpunkte</p>
+    </Layout>
+  );
+};
+
+export default Specialization;
+
+export const Head = () => <title>Schwerpunkte</title>;
