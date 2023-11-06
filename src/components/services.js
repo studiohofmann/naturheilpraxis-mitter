cfa773ec8e8001f0e7e2c8cba9b9149c106b62c2
First First Design <contact@firstfirst.design>
2023-11-06 19:11:25 +0100

    Tailwind.config

--- src/components/services.js
+++ src/components/services.js
@@ -1,6 +1,5 @@
 import * as React from "react";
 import { useStaticQuery, graphql } from "gatsby";
-import { Layout } from "gatsby-plugin-image";
 
 export default function About() {
   const data = useStaticQuery(graphql`
