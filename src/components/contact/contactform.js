6ff8a7e3136621244c9a8f7e080f9967bab341da
First First Design <contact@firstfirst.design>
2023-11-08 00:29:35 +0100

    Contact form

--- src/components/contact/contactform.js
+++ src/components/contact/contactform.js
@@ -2,33 +2,36 @@ import * as React from "react";
 
 const Contactform = () => {
   return (
-    <form name="contact" method="POST" data-netlify="true">
+    <form name="contact" method="POST" data-netlify="true" className="">
       <p>
-        <label>
-          Your Name: <input type="text" name="name" />
+        <label className="flex justify-between my-3 bg-lime-200">
+          <div className="m-8">Name</div>
+          <input className="w-6/12 bg-amber-400" type="text" name="name" />
         </label>
       </p>
       <p>
-        <label>
-          Your Email: <input type="email" name="email" />
+        <label className="flex justify-between my-3 bg-lime-200">
+          <div className="m-8">Email</div>
+          <input className="w-6/12 bg-amber-400" type="email" name="email" />
         </label>
       </p>
       <p>
-        <label>
-          Your Role:{" "}
-          <select name="role[]" multiple>
-            <option value="leader">Leader</option>
-            <option value="follower">Follower</option>
-          </select>
+        <label className="flex justify-between my-3 bg-lime-200">
+          <div className="m-8">Betreff</div>
+          <input className="w-6/12 bg-amber-400" type="email" name="email" />
+        </label>
+      </p>
+
+      <p>
+        <label className="flex justify-between my-3 bg-lime-200">
+          <div className="m-8">Nachricht</div>
+          <textarea className="w-6/12 bg-amber-400" name="message"></textarea>
         </label>
       </p>
       <p>
-        <label>
-          Message: <textarea name="message"></textarea>
-        </label>
-      </p>
-      <p>
-        <button type="submit">Send</button>
+        <button className="bg-cyan-500 w-full my-3" type="submit">
+          Senden
+        </button>
       </p>
     </form>
   );
