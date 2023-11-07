9a0838a94fe428195bdeb5e7e1bb5da0d0406c69
First First Design <contact@firstfirst.design>
2023-11-07 19:26:52 +0100

    idk

--- src/components/services.js
+++ src/components/services.js
@@ -1,21 +1,42 @@
 import * as React from "react";
 import { useStaticQuery, graphql } from "gatsby";
 
-export default function About() {
+export default function Services() {
   const data = useStaticQuery(graphql`
-    query AboutQuery {
-      contentfulAbout {
-        heading
-        portrait {
-          gatsbyImageData
-          title
+    query ServicesQuery {
+      allContentfulLeistungen(sort: { order: ASC }) {
+        edges {
+          node {
+            heading
+            order
+          }
         }
-        text {
-          text
+      }
+      allContentfulContentType(filter: { name: { eq: "Leistungen" } }) {
+        edges {
+          node {
+            name
+          }
         }
       }
     }
   `);
 
-  return <div>Services</div>;
+  return (
+    <div>
+      {data.allContentfulLeistungen.edges.map(({ node }, i) => {
+        return (
+          <div className="">
+            <h1 className="text-center">
+              {data.allContentfulLeistungen.edges.heading}
+            </h1>
+
+            <div key={i}>
+              <h2 className="mb-3">{node.heading}</h2>
+            </div>
+          </div>
+        );
+      })}
+    </div>
+  );
 }
