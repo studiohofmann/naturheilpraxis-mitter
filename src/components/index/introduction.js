07461dbf19250224cce527a1819e2173a163565c
First First Design <contact@firstfirst.design>
2023-11-08 19:58:21 +0100

    Nachtrag

--- src/components/index/introduction.js
+++ src/components/index/introduction.js
@@ -11,8 +11,8 @@ export default function Introduction() {
   `);
 
   return (
-    <h1 className="absolute z-20  top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 text-white">
+    <h2 className="absolute z-20  top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 text-white">
       {data.contentfulIndex.introduction}
-    </h1>
+    </h2>
   );
 }
